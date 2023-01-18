1bd02b1857fd8a919ed1f151d735f7af441a946a
AishaBukar <49945216+AishaBukar@users.noreply.github.com>
2023-01-18 10:19:20 +0100

    Create StringsAndVariables.js
    StringsAndVariables

---
+++ StringsAndVariables.js
@@ -0,0 +1,21 @@
+// Initializing a string to a variable using the var keyword
+var myName= "Aisha Bukar Awobadejo";
+
+// One drawback of var is the fact that its value can be overridden, so, instead we use the let keyword
+let myName= "Aisha Bukar Awobadejo";
+console.log(myName);
+
+//Constant values cannot be changed
+const myLastName ="Bukar Awobadejo";
+//Assigning numbers to variables and performing operations
+
+let myAdd = 5 + 10.5;
+let mySub = 4 - 2;
+let myMult = 9 * 5;
+let myDiv = 5 / 2;
+let myRem = 3 % 2;
+
+myDiv /=1;
+myAdd =+1;
+mySub -=5;
+myMult *=4;
