328ce4435c8620784bc583212107a3a6a11c82ef
AishaBukar <49945216+AishaBukar@users.noreply.github.com>
2023-02-28 15:52:04 +0100

    Create Arrays.js
    declaring arrays

---
+++ Arrays.js
@@ -0,0 +1,15 @@
+// Arrays can contain any data type like string, boolean, numbers, and so on.
+
+//Declaring an array with just strings
+myArray=["Apple", "Pineapple", "Melon"];
+
+mySecondArray=["Apple", "Pineapple", 2, 3];
+
+// Declaring nested arrays
+const myArray = [
+    ["Here is an Array ", 1],
+    ["Within another array ", 2],
+    ["I like to indent it like this for proper reading and understanding ", 3],
+    ["Cheerrsssssss ", "Aisha ", 2023]
+  
+  ];
